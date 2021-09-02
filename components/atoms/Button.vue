5fc796d7a86b41bb7c88576e77d095b0744407f8
andrea-emmanuele <contact@andreaemmanuele.it>
2021-09-02 11:16:19 +0200

    chore: storybook configuration

---
+++ components/atoms/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button">Button</button>
+</template>
+
+<script>
+export default {
+  name: 'my-button',
+}
+</script>
+
+<style lang='scss' scoped>
+button {
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <MyButton label="button" />
+</template>
+
+<script>
+import MyButton from '~/components/atoms/Button'
+export default {
+  components: { MyButton }
+}
+</script>
